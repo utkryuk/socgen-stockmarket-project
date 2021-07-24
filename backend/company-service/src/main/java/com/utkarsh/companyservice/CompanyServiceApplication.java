081103b7587622bdb8da39ca5ebb5b32560aa865
Utkarsh Gupta <utkryuk@gmail.com>
2021-07-24 21:57:09 +0530

    added swagger to the backend

--- backend/company-service/src/main/java/com/utkarsh/companyservice/CompanyServiceApplication.java
+++ backend/company-service/src/main/java/com/utkarsh/companyservice/CompanyServiceApplication.java
@@ -3,13 +3,43 @@ package com.utkarsh.companyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Company Microservice API",
+				"API Documentation for Company Microservice in Stock Market Application",
+				"1.0",
+				"Copyright - Utkarsh Gupta",
+				new Contact("Utkarsh Gupta", "https://github.com/utkryuk", "utkryuk@gmail.com"),
+				"API License",
+				"https://github.com/utkryuk/socgen-stockmarket-project"
+		);
+	}
+
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.build()
+				.apiInfo(apiDetails());
+
 	}
 
 }
